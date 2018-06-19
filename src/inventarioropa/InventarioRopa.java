5c9f2f3ce75bd252a9d4f01192d464c7e6a63561
NATANAEL MORENO <NATANAEL MORENO@NATANAELMORENO>
2018-06-19 11:22:42 -0600

    inicio del proyecto

---
+++ src/inventarioropa/InventarioRopa.java
@@ -0,0 +1,12 @@
+
+package inventarioropa;
+
+import javax.swing.JOptionPane;
+
+public class InventarioRopa {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null,"hola mundo");
+    }
+    
+}
