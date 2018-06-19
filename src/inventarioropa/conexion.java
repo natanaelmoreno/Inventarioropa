a0fda989db7118e5f3b5be2356e3e589f33152aa
NATANAEL MORENO <NATANAEL MORENO@NATANAELMORENO>
2018-06-19 11:29:40 -0600

    clase conexion

---
+++ src/inventarioropa/conexion.java
@@ -0,0 +1,52 @@
+
+package inventarioropa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class conexion {
+     
+      private String url = "jdbc:mysql://localhost:3306/yaeldb";
+    private String login = "root"; //Administrador de MySQL
+    private String password = "natanaelmoreno";
+    private Connection cnx = null;
+    private Statement sttm = null;
+    private ResultSet rst = null;
+    
+    //método para Update, Insert, Delete
+    public void UID(String sql) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, login, password);
+            sttm = cnx.createStatement();
+            sttm.executeUpdate(sql); //statement
+        } catch (ClassNotFoundException  c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1); //salir de aplicación
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+    //Método para Consultar
+    public ResultSet getValores(String sql) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, login, password);
+            sttm = cnx.createStatement();
+            rst = sttm.executeQuery(sql);  //resultset
+        } catch (ClassNotFoundException c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + e.getMessage());
+            System.exit(1);
+        } 
+        return rst;
+        
+    }
+}
